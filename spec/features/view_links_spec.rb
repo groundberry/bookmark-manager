933dc9ec9b0d4a2dc9c98b71e722bab2d2ede4f7
Laura Weston <laura.weston@outlook.com>
2016-10-18 15:48:47 +0100

    Add view links feature test and create Link model

---
+++ spec/features/view_links_spec.rb
@@ -0,0 +1,14 @@
+require './app/models/link'
+
+feature "Viewing links" do
+  scenario "existing links can be viewed on the page" do
+    Link.create(url: "http://makersacademy.com", title: "Makers Academy")
+
+    visit "/links"
+    expect(page.status_code).to eq 200
+
+    within "ul#links" do
+      expect(page).to have_content("Makers Academy")
+    end
+  end
+end
